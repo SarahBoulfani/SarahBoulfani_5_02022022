f82fadf9da5e9a535f56cac738bc86df634a5f62
Sarah Boulfani <boulfanisarah@gmail.com>
2022-02-15 12:47:27 +0100

    Affichage titre, prix et description du produit ok

--- front/js/product.js
+++ front/js/product.js
@@ -12,7 +12,7 @@ async function getProduct(){
             return res.json() 
           
         }
-        console.log(res) //ceci affiche le produit en question 
+        console.log(res) //ceci affiche  les différentes informations du produit en question 
     })
     .then(function(data){
         product = data; 
@@ -23,4 +23,43 @@ async function getProduct(){
         console.log(err)
     })
     }
-    getProduct();
+  
+
+    //Selectionner la classe ou je vais injecter le code HTML
+    //image
+    const item = document.querySelector(".item")
+    console.log(item);
+    const item__img = document.querySelector(".item__img");
+    console.log(item__img);
+
+    //price
+  /*   const item__content = document.querySelector(".item__content");
+    const item__content__titlePrice = document.querySelector(".item__content__titlePrice"); */
+    const titlePrice = document.getElementById("title");
+    const price = document.getElementById("price");
+    
+    //description:
+    const description = document.getElementById("description");
+    //Afficher le produit dans la page Produit (dans le DOM) avec la fonction productDisplay
+    async function productDisplay(){
+    await getProduct();
+    //image
+    let image = document.createElement("img");
+    image.src = `${product.imageUrl}`;
+    image.alt = `${product.altTxt}`;
+    item__img.appendChild(image);
+ /* deuxiéme methode:
+    item__img.innerHTML =  `<img src="${product.imageUrl}" alt=${product.altTxt}> `; */
+   
+    //Price
+   titlePrice.textContent = `${product.name}`;
+   price.textContent = `${product.price}`;
+   
+   //description
+  description.textContent = `${product.description}`;
+    
+   
+
+
+    }
+productDisplay();
