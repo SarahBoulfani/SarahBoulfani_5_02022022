5e70fa5e3eae50427a8072184fae4b33c81a4f66
Sarah Boulfani <boulfanisarah@gmail.com>
2022-02-16 15:18:37 +0100

    Ajouter des commentaires

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,9 @@
-//Récupèrer l'id produit depuis l'Url avec URLSearchParams
+/*Récupèrer l'id produit depuis l'Url avec URLSearchParams*/
 let params = new URLSearchParams(document.location.search);
 let productID = params.get("id");
 console.log(productID); 
 
-//Récupérer mon produit depuis l'API grâce à son Id
+/*Récupérer mon produit depuis l'API grâce à son Id*/
 let product = [];
 async function getProduct(){
     await fetch(`http://localhost:3000/api/products/${productID}`)
@@ -12,7 +12,6 @@ async function getProduct(){
             return res.json() 
           
         }
-        console.log(res) //ceci affiche  les différentes informations du produit en question 
     })
     .then(function(data){
         product = data; 
@@ -25,25 +24,26 @@ async function getProduct(){
     }
   
 
-    //Selectionner l'id ou les classes ou je vais injecter le code HTML
+ /*Récupérer les élémnents avec l'id ou leur classe pour créer et afficher le produit en question dans la page produit*/
     //image
     const item__img = document.querySelector(".item__img");
-    console.log(item__img);
-
+   
+    //Titre du produit
+    const title = document.getElementById("title");
+    
     //price
-    const titlePrice = document.getElementById("title");
     const price = document.getElementById("price");
     
     //description:
     const description = document.getElementById("description");
 
     //couleur
-    const colors = document.getElementById("colors");
+    const selectColor = document.getElementById("colors");
 
-    //Afficher le produit en question dans la page produit (dans le DOM) avec la fonction productDisplay
+ /*Afficher le produit en question dans la page produit (dans le DOM) avec la fonction productDisplay*/ 
     async function productDisplay(){
     await getProduct();
-    //image
+//image du produit
     let image = document.createElement("img");
     image.src = product.imageUrl;
     image.alt = product.altTxt;
@@ -51,19 +51,22 @@ async function getProduct(){
  /* deuxiéme methode:
     item__img.innerHTML =  `<img src="${product.imageUrl}" alt=${product.altTxt}> `; */
    
-//Price
-    titlePrice.textContent = product.name;
+//Titre du produit
+    title.textContent = product.name;
+
+//Prix du produit
     price.textContent = product.price;
    
-//description
+//description du produit
     description.textContent = product.description;
    
-//couleur
+//une boucle pour selectionner la couleur  
     for (let i = 0; i < product.colors.length; i++){
+    const selectColor = document.getElementById("colors");
     let option = document.createElement("option");
     option.value = product.colors[i];
     option.textContent =  product.colors[i];
-    colors.appendChild(option); 
+    selectColor.appendChild(option); 
     console.log(option)
 } 
 }
