6d439587169b69f10b7a4dee9bd078e0da95a173
Sarah Boulfani <boulfanisarah@gmail.com>
2022-03-03 20:02:34 +0100

    probléme avec la quantité null

--- front/js/product.js
+++ front/js/product.js
@@ -1,159 +1,146 @@
 /*Récupèrer l'id produit depuis l'Url avec URLSearchParams*/
 let params = new URLSearchParams(document.location.search);
 let productID = params.get("id");
-console.log(productID); 
+console.log(productID);
 
-/*Récupérer mon produit depuis l'API grâce à son Id*/
+/*Fonction qui récupérer le produit depuis l'API grâce à son Id*/
 let product = [];
-async function getProduct(){
-    await fetch(`http://localhost:3000/api/products/${productID}`)
-    .then(function(res){
-        if(res.ok){
-            return res.json() 
-          
-        }
-    })
-    .then(function(data){
-        product = data; 
-      console.log(product) 
-    })
-    
-    .catch(function(err){
-        console.log(err)
-    })
-    }
-  
+async function getProduct() {
+   await fetch(`http://localhost:3000/api/products/${productID}`)
+      .then(function (res) {
+         if (res.ok) {
+            return res.json()
 
- /*Récupérer les élémnents HTML avec l'id ou leur classe pour créer et afficher le produit en question dans la page produit*/
-    //image
-    const item__img = document.querySelector(".item__img");
-   
-    //Titre du produit
-    const title = document.getElementById("title");
-    
-    //price
-    const price = document.getElementById("price");
-    
-    //description:
-    const description = document.getElementById("description");
+         }
+      })
+      .then(function (data) {
+         product = data;
+         console.log(product)
+      })
 
-    //couleur
-    const selectColor = document.getElementById("colors");
-    //Quantité
-    const quantity = document.querySelector("#quantity");
+      .catch(function (err) {
+         console.log(err)
+      })
+}
 
- /*Afficher le produit en question dans la page produit (dans le DOM) avec la fonction productDisplay*/ 
-    async function productDisplay(){
-    await getProduct();
- //image du produit
-    let image = document.createElement("img");
-    image.src = product.imageUrl;
-    image.alt = product.altTxt;
-    item__img.appendChild(image);
- /* deuxiéme methode:
-    item__img.innerHTML =  `<img src="${product.imageUrl}" alt=${product.altTxt}> `; */
-   
- //Titre du produit
-    title.textContent = product.name;
 
- //Prix du produit
-    price.textContent = product.price;
-   
- //description du produit
-    description.textContent = product.description;
-   
- //une boucle pour selectionner la couleur : pour dire crée pour le parent selectColor tant d'option quil faut donc tout dépend de la longeur de mon tableau colors avec comme valeur et contenu product.colors[i] 
-    for (let i = 0; i < product.colors.length; i++){
-    const selectColor = document.getElementById("colors");
-    let option = document.createElement("option");
-    option.value = product.colors[i];
-    option.textContent =  product.colors[i];
-    selectColor.appendChild(option); 
-} 
+/*Récupérer les élémnents HTML avec l'id ou leur classe pour créer et afficher le produit en question dans la page produit*/
+//Image
+const item__img = document.querySelector(".item__img");
+
+//Titre du produit
+const title = document.getElementById("title");
+
+//Price
+const price = document.getElementById("price");
+
+//Description:
+const description = document.getElementById("description");
+
+//Couleur
+const selectColor = document.getElementById("colors");
+//Quantité
+const quantity = document.querySelector("#quantity");
+
+/*Fonction qui affiche le produit en question dans la page produit*/
+async function productDisplay() {
+   await getProduct();
+   //image du produit
+   let image = document.createElement("img");
+   image.src = product.imageUrl;
+   image.alt = product.altTxt;
+   item__img.appendChild(image);
+   //Titre du produit
+   title.textContent = product.name;
+   //Prix du produit
+   price.textContent = product.price;
+   //description du produit
+   description.textContent = product.description;
+   //une boucle pour selectionner la couleur : pour dire crée pour le parent selectColor tant d'option quil faut donc tout dépend de la longeur de mon tableau colors avec comme valeur et contenu product.colors[i] 
+   for (let i = 0; i < product.colors.length; i++) {
+      const selectColor = document.getElementById("colors");
+      let option = document.createElement("option");
+      option.value = product.colors[i];
+      option.textContent = product.colors[i];
+      selectColor.appendChild(option);
+   }
 }
 productDisplay();
-
-//________________________________________________
-//Afficher la quantité dans la console 
-/* console.log(quantity);
-
-quantity.addEventListener('change', (event)=>{
-     choiceQuantity = event.target.value; 
-     console.log(choiceQuantity);//ceci affiche la quantité saisie dans l'iput quantité
-}); */
-//________________________________________________
 //-----------------------------La gestion du panier------------------------------
 /*Récupération des données selectionnées par l'utilisateur et envoie du panier*/
-function addBasket(){
-//Selectionner l'id du bouton ajouter au panier
-const addToCart = document.querySelector("#addToCart");
-console.log(addToCart);
+/*Fonction pour ajouter les produits dans le panier */
+function addBasket() {
+   //Selectionner l'id du bouton ajouter au panier
+   const addToCart = document.querySelector("#addToCart");
+   console.log(addToCart);
 
+   //Ecouter le bouton ajouter au panier
+   addToCart.addEventListener('click', () => {
+      //Récupérer les valeur selectionnées par l'utilisateur
+      let choiceProduct = {
+         id: productID,
+         colors: selectColor.value,
+         quantity: parseInt(quantity.value)
+      }
+      console.log(choiceProduct);
 
-//Ecouter le bouton ajouter au panier
-addToCart.addEventListener('click',(event)=>{
-//Récupérer les valeur selectionnées par l'utilisateur
-let choiceProduct = {
-    id : productID,
-    colors: selectColor.value,
-    quantity : parseInt(quantity.value) //parseInt pour l'afficher en nombre et pas en chaine de caractére
-}
-  console.log(choiceProduct);
- 
-   //Stocker la recupération des valeur sélectionnées par l'utilisateur
-  //Déclarer la variable qui va contenir les clés et valeurs qui sont dans le localStorage
-  let addLocalStorage = JSON.parse( localStorage.getItem("productAdded")) ;//JSON.parse() pour convertir les données au format JSON qui sont dans le locaStorage en objet JavaScript 
+      //Stocker la recupération des valeur sélectionnées par l'utilisateur
+      //Déclarer la variable qui va contenir les clés et valeurs qui sont dans le localStorage
+      let addLocalStorage = JSON.parse(localStorage.getItem("productAdded"));//JSON.parse() pour convertir les données au format JSON qui sont dans le locaStorage en objet JavaScript 
 
-    //fonction ajouter un produit au localStorage:
-    function addToLocalStorage(){
-        addLocalStorage.push(choiceProduct);//La méthode push() pour ajouter le produit selectionné par l'utilisateur (choiceProduct) dans un tableau (addLocalStorage)
-        localStorage.setItem("productAdded", JSON.stringify(addLocalStorage));//la transformation en format JSON et l'envoyer dans la key "productAdded" au localStorage
-     };
-     //Fonction qui traite les deux possibilités
-     function storage(){
+      /*Fonction ajouter un produit au localStorage*/
+      function addToLocalStorage() {
+         addLocalStorage.push(choiceProduct);//La méthode push() pour ajouter le produit selectionné par l'utilisateur (choiceProduct) dans un tableau (addLocalStorage)
+         localStorage.setItem("productAdded", JSON.stringify(addLocalStorage));//La transformation en format JSON et l'envoyer dans la key "productAdded" au localStorage
+      };
+      /*Fonction qui traite les deux possibilités*/
+      function storage() {
          //Nous avons deux possibilité
-     /*-----(1)-S'il y a déja un produit d'enregistré dans le localStorage-----*/
-     if(addLocalStorage !== null){   
-        let foundProduct = addLocalStorage.find(p => p.id == choiceProduct.id && choiceProduct.colors == p.colors) //find est une fonction qui travaille sur les tableau et qui permet de chercher un elelment sur un tableau par rapport à une condition, find() si elle trouve l'element elle va retourner l'element en question sinon elle retourne undefined 
-        if (foundProduct != undefined){
-         foundProduct.quantity =  choiceProduct.quantity + foundProduct.quantity;
-         //mise à jour du panier
-         localStorage.setItem("productAdded", JSON.stringify(addLocalStorage));
-        }else if(addLocalStorage){ //sinon s'il y a deja un produit avec id et couleur différents on crée un nouveau objet dans le panier
-            addToLocalStorage();
-        }
-        console.log(addLocalStorage);//affiche un tableau avec tous les produits selectionnés par l'utilisateur(choiceProduct)
-       
-      //-----(2)-S'il n'y a pas de produit d'enregistré dans le localStorage alors on crée le premier objet
-    }else{  
-        addLocalStorage = [];
-        addToLocalStorage(); 
-     }
-    }
-      //Fonction confirmation fenetre pop up:
-  function popupConfirmation(){
-    if(window.confirm(`${product.name} 
-    Couleur : ${selectColor.value} , Quantité : ${quantity.value} a bien été ajouter au panier 
-    Consulter le panier Ok ou revenir à l'accueil Annuler `  )){
-    window.location.href = "cart.html";
-    }else{
-       window.location.href = "index.html";
-    }
-  };
+         /*---(1)-S'il y a déja un produit d'enregistré dans le localStorage-----*/
+         if (addLocalStorage !== null) {
+            let foundProduct = addLocalStorage.find(p => p.id == choiceProduct.id && choiceProduct.colors == p.colors) //find est une fonction qui travaille sur les tableau et qui permet de chercher un elelment sur un tableau par rapport à une condition, find() si elle trouve l'élement elle va retourner l'élement en question sinon elle retourne undefined 
+            if (foundProduct != undefined) {  //Si on trouve le même produit dans le panier alors on augmente sa quantité 
+               foundProduct.quantity = choiceProduct.quantity + foundProduct.quantity;
+               //mise à jour du panier
+               localStorage.setItem("productAdded", JSON.stringify(addLocalStorage));
+            } else if (addLocalStorage) { //sinon s'il y a deja un produit avec id et couleur différents on crée un nouveau objet dans le panier
+               addToLocalStorage();
+            }
+            console.log(addLocalStorage);//affiche un tableau avec tous les produits selectionnés par l'utilisateur(choiceProduct)
 
-  //On vérifie si une quantité et une couleur ont bien été choisies
-   if(choiceProduct.quantity == 0 || choiceProduct.quantity >100 || choiceProduct.colors == "" ){
-       alert("Veuillez indiquer une quantité correcte et choisir une couleur");
-       
-   }else{//sinon on traite les deux possibilité
-     popupConfirmation();
-     storage();  
-   }
-});
+            /*---(2)-S'il n'y a pas de produit d'enregistré dans le localStorage alors on crée le premier objet*/
+         } else {
+            addLocalStorage = [];
+            addToLocalStorage();
+         }
+      }
+      /*Fonction confirmation fenetre pop up*/
+      function popupConfirmation() {
+         if (window.confirm(`${product.name} 
+    Couleur : ${selectColor.value} , Quantité : ${quantity.value} a bien été ajouter au panier 
+    Consulter le panier Ok ou revenir à l'accueil Annuler `  )) {
+            window.location.href = "cart.html";
+         } else {
+            window.location.href = "index.html";
+         }
+      };
+
+      /*Fonction qui vérifie si une quantité et une couleur ont bien été choisies*/
+      function verifInput() {//on affiche une alerte pour renseigner les input
+         if (choiceProduct.quantity == 0 ||  choiceProduct.quantity == null ||  choiceProduct.quantity > 100 || choiceProduct.colors == "") {
+            alert("Veuillez indiquer une quantité correcte et choisir une couleur");
+
+         } else {//Sinon on affiche une popup et on ajoute le produit au localStorage avec la fonction storage
+            popupConfirmation();
+            storage(); 
+         }
+      }
+      verifInput();
+   });
 }
 addBasket();
 
 
 
 
- 
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,52 +1,52 @@
 //Initialiser la variable qui contiendra nos données API
 let articles = [];
 /*Fonction qui récupére les produits depuis l'API*/
-async function getArticles(){
-await fetch('http://localhost:3000/api/products')//await pour attendre que ce code s'exécute pour passer au suivant et du coup avoir du temps pour passer les données dans notre variable
-.then(function(res){//Appeler la fonction then pour récupérer la réponse de la requete 
-    if(res.ok){
-      //  console.log(res) Ceci affiche que nous avons une réponse 
+async function getArticles() {
+  await fetch('http://localhost:3000/api/products')//await pour attendre que ce code s'exécute pour passer au suivant et du coup avoir du temps pour passer les données dans notre variable
+    .then(function (res) {//Appeler la fonction then pour récupérer la réponse de la requete 
+      if (res.ok) {
+        //  console.log(res) Ceci affiche que nous avons une réponse 
         return res.json() //Réponse sous format json
-    }
-})
-.then(function(data){//Récuperer la réponse dans la fonction then suivante puis l'afficher dans la console
-    articles = data; //Stocker nos données dans la variable articles 
-    console.log(articles) 
-})
-//
-.catch(function(err){
-    console.log(err)
-})
+      }
+    })
+    .then(function (data) {//Récuperer la réponse dans la fonction then suivante puis l'afficher dans la console
+      articles = data; //Stocker nos données dans la variable articles 
+      console.log(articles)
+    })
+    //
+    .catch(function (err) {
+      console.log(err)
+    })
 }
 /*Fonction qui affiche dynamiquement les articles sur notre page d'accueil*/
-async function articlesDisplay(){
-  await  getArticles(); //Appeler notre fonction qui récupére les données
+async function articlesDisplay() {
+  await getArticles(); //Appeler notre fonction qui récupére les données
 
-  for(let article of articles){
+  for (let article of articles) {
     let items = document.getElementById("items");//Récupérer mon élément avec l'id items
-    //link
+    //Link
     let link = document.createElement("a");//Créer un element a
     link.href = `./product.html?id=${article._id}`; // Pour chaque article dans mon tableau articles je récupére la valeur de ma clé "_id"
     items.appendChild(link);//Afficher l'élement sur ma page web grace à la fonction appendChild
-    //article
+    //Article
     let cart = document.createElement("article");
     link.appendChild(cart);
-    //img
+    //Img
     let image = document.createElement("img");
     image.src = article.imageUrl;
     image.alt = article.altTxt;
     cart.appendChild(image);
-    //titre
+    //Titre
     let productName = document.createElement("h3");
     productName.textContent = article.name;
     productName.classList.add("productName");
     cart.appendChild(productName);
-    //description
+    //Description
     let productDescription = document.createElement("p");
     productDescription.textContent = article.description;
     productDescription.classList.add("productDescription");
     cart.appendChild(productDescription);
-  } 
+  }
 }
 articlesDisplay();
 
